8231f632ceeb2139d953cb359efb300c2c90ea04
Harshal k <99262345+harshk1234@users.noreply.github.com>
2022-03-29 10:50:01 +0530

    Update 3)where.sql

--- 3)where.sql
+++ 3)where.sql
@@ -1,7 +1,14 @@
-Pulls the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or equal to 1000.
+1)Pulls the first 5 rows and all columns from the orders table that have a dollar amount of gloss_amt_usd greater than or equal to 1000.
 
 SELECT *
 FROM orders
 WHERE gloss_amt_usd >= 1000
 LIMIT 5;
 You will notice when using these WHERE statements, we do not need to ORDER BY unless we want to actually order our data. Our condition will work without having to do any sorting of the data.
+
+2)Filter the accounts table to include the company name, website, and the primary point of contact (primary_poc) just for the Exxon Mobil company in the accounts table.
+SELECT name, website, primary_poc
+FROM accounts
+WHERE name = 'Exxon Mobil';
+
+
